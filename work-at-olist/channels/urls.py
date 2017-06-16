ce20b6b38cc77435645debdffee0cd7820756607
Bruno Soares <brunomsss@gmail.com>
2017-06-16 15:06:25 -0300

    Added a name to ChannelList url

--- work-at-olist/channels/urls.py
+++ work-at-olist/channels/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import ChannelList
 
 urlpatterns = [
-    url(r'^channels/$', ChannelList.as_view())
+    url(r'^channels/$', ChannelList.as_view(), name='channels-list'),
 ]
