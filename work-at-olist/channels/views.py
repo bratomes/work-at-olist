cbdf2e5e7fe64283c2d55c9950bd4ad5054e3149
Bruno Soares <brunomsss@gmail.com>
2017-06-16 14:06:40 -0300

    Created ChannelList view

--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Channel
+from .serializers import ChannelSerializer
+
+
+class ChannelList(APIView):
+    """
+    List all channels
+    """
+    def get(self, request, format=None):
+        channels = Channel.objects.all()
+        serializer = ChannelSerializer(channels, many=True)
+        return Response(serializer.data)
