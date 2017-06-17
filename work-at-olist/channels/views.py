aa8c9d6f53e5c522a08f55a580eb0adc0929413a
Bruno Soares <brunomsss@gmail.com>
2017-06-17 14:45:39 -0300

    Modified CategoryList to return 404 if no channel filter is specified

--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import generics
 
 from .models import Channel, Category
 from .serializers import ChannelSerializer, CategorySerializer
@@ -17,17 +16,17 @@ class ChannelList(APIView):
         return Response(serializer.data)
 
 
-class CategoryList(generics.ListAPIView):
+class CategoryList(APIView):
     """
     List all categories filtered by a channel. If no channel filter is provided
-    will return None
+    will return 404
     """
-    serializer_class = CategorySerializer
-
-    def get_queryset(self):
-        queryset = None
+    def get(self, requeset, format=None):
         channel = self.request.query_params.get('channel', None)
-        if channel is not None:
-            queryset = Category.objects.filter(channel__name=channel)
 
-        return queryset
+        if channel is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        categories = Category.objects.filter(channel__name=channel)
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
