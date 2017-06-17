d5095fcb32f39728600349c3357a1bd15ef00b1a
Bruno Soares <brunomsss@gmail.com>
2017-06-17 15:52:02 -0300

    Created url to CategoryDetail view

--- work-at-olist/channels/urls.py
+++ work-at-olist/channels/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from .views import ChannelList, CategoryList
+from .views import ChannelList, CategoryList, CategoryDetail
 
 urlpatterns = [
     url(r'^channels/$', ChannelList.as_view(), name='channels-list'),
     url(r'^categories/$', CategoryList.as_view(), name='categories-list'),
+    url(r'^categories/(?P<pk>[0-9A-Za-z]+)/$', CategoryDetail.as_view(),
+        name='category-detail'
+    ),
 ]
