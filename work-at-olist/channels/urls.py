763c64bedc1b44cc5a2df7f6e0a225c5d430d0c3
Bruno Soares <brunomsss@gmail.com>
2017-06-17 13:21:14 -0300

    Added url to CategoryList view

--- work-at-olist/channels/urls.py
+++ work-at-olist/channels/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import ChannelList
+from .views import ChannelList, CategoryList
 
 urlpatterns = [
     url(r'^channels/$', ChannelList.as_view(), name='channels-list'),
+    url(r'^categories/$', CategoryList.as_view(), name='categories-list'),
 ]
