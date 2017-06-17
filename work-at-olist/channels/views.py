11a56b3c4498657b8d60795dddebc359ca8a47bd
Bruno Soares <brunomsss@gmail.com>
2017-06-17 15:51:45 -0300

    Created CategoryDetail view

--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -1,9 +1,12 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Channel, Category
-from .serializers import ChannelSerializer, CategorySerializer
+from .serializers import (
+    ChannelSerializer, CategorySerializer, CategoryDetailSerializer
+)
 
 
 class ChannelList(APIView):
@@ -29,4 +32,26 @@ class CategoryList(APIView):
 
         categories = Category.objects.filter(channel__name=channel)
         serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+
+class CategoryDetail(APIView):
+    """
+    Retrieve a category instance of a channel, with its parent and subcategories
+    """
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+
+        channel = self.request.query_params.get('channel', None)
+
+        if channel is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        category = self.get_object(pk)
+        serializer = CategoryDetailSerializer(category)
         return Response(serializer.data)
