260f2f141d8d07d2c7b6d017ca52b05eb85245c2
Bruno Soares <brunomsss@gmail.com>
2017-06-18 00:55:02 -0300

    Added silk urls

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('channels.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
