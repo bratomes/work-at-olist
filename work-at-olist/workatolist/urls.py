0dab6b98e99702426d57f18206f73dcd2f0543fe
Bruno Soares <brunomsss@gmail.com>
2017-06-19 17:56:12 -0300

    Included rest_framework_docs urls and version 1 api urls

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('channels.urls'))
+    url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^api/v1/', include('channels.urls.urls_v1'))
 ]
 
 if settings.DEBUG:
