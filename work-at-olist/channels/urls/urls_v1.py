76f1d5bd151496c98c9d53c2d4b1bc63754c09b7
Bruno Soares <brunomsss@gmail.com>
2017-06-26 12:02:24 -0300

    Modified channel list url name

--- work-at-olist/channels/urls/urls_v1.py
+++ work-at-olist/channels/urls/urls_v1.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from channels.views import ChannelList, ChannelDetail, CategoryDetail
 
 urlpatterns = [
-    url(r'^channels/$', ChannelList.as_view(), name='channels-list'),
+    url(r'^channels/$', ChannelList.as_view(), name='channel-list'),
     url(r'^channels/(?P<slug>[A-Za-z\-_]+)/$',
         ChannelDetail.as_view(), name='channel-detail'),
     url(r'^categories/(?P<pk>[0-9A-Za-z]+)/$',
