8e9e4ca46cc7b81d2b883fc54bae933a5fcf13b9
Bruno Soares <brunomsss@gmail.com>
2017-06-26 22:28:03 -0300

    Removed model_mommy and used channels fixture

--- work-at-olist/channels/tests/test_views.py
+++ work-at-olist/channels/tests/test_views.py
@@ -1,42 +1,39 @@
 from django.urls import reverse
-from django.conf import settings
 from rest_framework.test import APITestCase
 from rest_framework import status
-from model_mommy import mommy
 
 from channels.models import Category
 
 
 class ChannelListTest(APITestCase):
+    fixtures = ['channels.json']
+
     def setUp(self):
         self.url = reverse('channel-list')
-        self.channel = mommy.make('channels.Channel', name='Wallmart')
 
     def test_get_channels_list(self):
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('Wallmart' in response.data[0]['name'])
+        self.assertEqual(response.data[0]['name'], 'Wallmart')
 
 
 class ChannelDetailTest(APITestCase):
+    fixtures = ['channels.json']
+
     def setUp(self):
         self.url = reverse('channel-detail', kwargs={'slug': 'wallmart'})
-        self.channel = mommy.make('channels.Channel', name='Wallmart')
-        mommy.make('channels.Category', _quantity=15, channel=self.channel)
 
     def test_get_channel_detail(self):
         response = self.client.get(self.url)
 
         self.assertTrue(status.is_success(response.status_code))
         self.assertEqual(response.data[0]['name'], 'Wallmart')
-        self.assertEqual(len(response.data[0]['categories']), 15)
+        self.assertEqual(len(response.data[0]['categories']), 23)
 
 
 class CategoryDetailTest(APITestCase):
-    def setUp(self):
-        self.channel = mommy.make('channels.Channel', name='Wallmart')
-        mommy.make('channels.Category', _quantity=15, channel=self.channel)
+    fixtures = ['channels.json']
 
     def test_get_category_by_hashid(self):
         category = Category.objects.all()[2]
