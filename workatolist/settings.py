949d1ba69a352729ef354c72993a1aa56be5b0f6
Bruno Soares <brunomsss@gmail.com>
2017-06-27 16:59:42 -0300

    Modified silk settings and url

--- workatolist/settings.py
+++ workatolist/settings.py
@@ -17,8 +17,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=config.boolean)
 
-TESTING = 'test' in sys.argv
-
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=config.list)
 
 
--- workatolist/urls.py
+++ workatolist/urls.py
@@ -4,9 +4,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^silk/', include('silk.urls', namespace='silk')),
     url(r'^docs/', include('rest_framework_docs.urls')),
     url(r'^api/v1/', include('channels.urls.urls_v1'))
 ]
-
-if settings.DEBUG or settings.TESTING:
-    urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
